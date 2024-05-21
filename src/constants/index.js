b3b809d8c1590e525815722bb9433948293214c5
Borys Meshkov <100526181+BorysMeshkov@users.noreply.github.com>
2024-05-21 21:07:43 +0300

    add simple express server

---
+++ src/constants/index.js
@@ -0,0 +1,3 @@
+export const ENV_VARS = {
+  PORT: 'PORT',
+};
---
+++ src/server.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import { env } from './utils/env.js';
+import { ENV_VARS } from './constants/index.js';
+import { errorHandlerMiddleware } from './middlewares/errorHandlerMiddleware.js';
+import { notFoundMiddleware } from './middlewares/notFoundMiddleware.js';
+
+export const startServer = () => {
+  const app = express();
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.use(cors());
+
+  app.get('/world', (req, res) => {
+    res.send('Hello world');
+  });
+
+  app.get('/error', (req, res, next) => {
+    next(new Error('some error here'));
+  });
+
+  app.use(notFoundMiddleware);
+
+  app.use(errorHandlerMiddleware);
+
+  const PORT = env(ENV_VARS.PORT, 3000);
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}!`);
+  });
+};
