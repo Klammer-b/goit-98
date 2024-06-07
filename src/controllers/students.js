24d10e4f8a8ec25003b1f00303e90ff1f08844e3
Borys Meshkov <100526181+BorysMeshkov@users.noreply.github.com>
2024-06-07 08:59:59 +0300

    add pagination, filtration, sort

--- src/controllers/students.js
+++ src/controllers/students.js
@@ -5,9 +5,21 @@ import {
   getStudentById,
   upsertStudent,
 } from '../services/students.js';
+import { parseFilters } from '../utils/parseFilters.js';
+import { parsePaginationParams } from '../utils/parsePaginationParams.js';
 
 export const getStudentsController = async (req, res) => {
-  const students = await getAllStudents();
+  const { page, perPage } = parsePaginationParams(req.query);
+  const { sortBy, sortOrder } = req.query;
+  const filter = parseFilters(req.query);
+
+  const students = await getAllStudents({
+    page,
+    perPage,
+    sortBy,
+    sortOrder,
+    filter,
+  });
 
   res.json({
     status: 200,
